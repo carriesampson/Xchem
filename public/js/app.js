ef65c386fc36789544913f7126d2c289b29f386e
carriesampson <i.carrie.sampson@gmail.com>
2018-01-27 14:53:37 -0500

    Created GET routes for Brands and Products

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,30 @@ const app = angular.module('xchem_app', ['ngRoute']);
 app.controller("bodyController", ['$http', function($http) {
   this.test = "IT WORKS!!";
   this.brains = [];
+  this.brands = [];
 
   $http({
     method: 'GET',
-    url: 'http://localhost:3000/brains/1'
+    url: 'http://localhost:3000/brains'
   }).then(function(response) {
     console.log(response);
-    this.brains.push(response.data);
+    this.brains = response.data;
+  }.bind(this));
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/brands'
+  }).then(function(response) {
+    console.log(response);
+    this.brands = response.data;
+  }.bind(this));
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/products'
+  }).then(function(response) {
+    console.log(response);
+    this.products = response.data;
   }.bind(this));
 
 }]);
